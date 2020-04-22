045081f310d4da9b7e0ad406e3c75921a9a3b900
Pablo Romano <pablo.romano42@gmail.com>
2020-04-22 08:20:27 -0500

    Math utilities function

---
+++ sampy/math.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def _handle_zeros_in_scale(scale, copy=True):
+	''' Makes sure that whenever scale is zero, we handle it correctly.
+	This happens in most scalers when we have constant features.
+
+	This is copied from scikit-learn under LICENSE
+
+	see: https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/preprocessing/_data.py
+	'''
+
+	# if we are fitting on 1D arrays, scale might be a scalar
+	if np.isscalar(scale):
+		if scale == .0:
+			scale = 1.
+		return scale
+	elif isinstance(scale, np.ndarray):
+		if copy:
+			# New array to avoid side-effects
+			scale = scale.copy()
+		scale[scale == 0.0] = 1.0
+		return
+
+
+def logn(X, base=10):
+	return np.log(X) / np.log(base)
