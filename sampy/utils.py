3f25697705966012019ae823175cfe1a3892b8d4
Pablo Romano <pablo.romano42@gmail.com>
2020-04-29 08:44:11 -0500

    added docstring to check_array

--- sampy/utils.py
+++ sampy/utils.py
@@ -8,6 +8,21 @@ __all__ = [
 
 
 def check_array(X, squeeze=False):
+	"""[summary]
+
+	Parameters
+	----------
+	X : array-like or numeric
+		The input data to standardize as a numpy array
+	squeeze : bool, optional
+		Whether or not the array should be "squeezed". This results in 
+		flattening all size 1 dimensions, by default False.
+
+	Returns
+	-------
+	numpy.ndarray
+		The cleaned data as a numpy.ndarray
+	"""
 	try:
 		return np.asscalar(X)
 	except:
