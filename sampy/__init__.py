f993aeaf32118ea634974f677636c7094123cb0b
Pablo Romano <pablo.romano42@gmail.com>
2020-05-08 08:22:36 -0500

    Added Discrete Uniform Distribution

--- sampy/__init__.py
+++ sampy/__init__.py
@@ -6,6 +6,7 @@ from .normal import Normal
 from .normal_half import HalfNormal
 from .poisson import Poisson
 from .uniform import Uniform
+from .uniform_discrete import DiscreteUniform
 
 from .interval import Interval
 from . import utils
