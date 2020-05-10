8a4878ac7375446179c5bbab3a4c474faeee8cba
Pablo Romano <pablo.romano42@gmail.com>
2020-05-10 16:14:31 -0500

    Added Chi2 distribution

---
+++ sampy/chi_squared.py
@@ -0,0 +1,58 @@
+import numpy as np
+import scipy.special as sc
+
+from sampy.gamma import Gamma
+from sampy.utils import check_array
+
+
+class Chi2(Gamma):
+
+	def __init__(self, dof=1, seed=None):
+		self.dof = dof
+		super(Chi2, self).__init__(0.5 * dof, 0.5, seed=seed)
+
+	@property
+	def mean(self):
+		return float(self.dof)
+
+	@property
+	def median(self):
+		return self.quantile(0.5)
+
+	@property
+	def mode(self):
+		return max([self.dof - 2.0, 0.0])
+
+	@property
+	def variance(self):
+		return 2.0 * self.dof
+
+	@property
+	def skewness(self):
+		return np.sqrt(8 / self.dof)
+
+	@property
+	def kurtosis(self):
+		return 12.0 / self.dof
+
+	@property
+	def entropy(self):
+		# reduced degrees of freed
+		k = self.dof / 2.0
+		return k + np.log(2 * sc.gamma(k)) + (1 - k) * sc.psi(k)
+
+	@property
+	def perplexity(self):
+		return np.exp(self.entropy)
+
+	@property
+	def support(self):
+		if self.dof == 1:
+			return Interval(0, np.inf, False, False)
+		return Interval(0, np.inf, True, False)
+
+	def __str__(self):
+		return f"Chi2(dof={self.dof})"
+
+	def __repr__(self):
+		return self.__str__()
