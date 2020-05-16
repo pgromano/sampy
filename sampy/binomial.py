7faf58076af1ffdf203fb1e09d5ba188fd8559ac
Pablo Romano <pablo.romano42@gmail.com>
2020-05-16 15:25:48 -0500

    Updaed formatting for binomial distribution

--- sampy/binomial.py
+++ sampy/binomial.py
@@ -9,173 +9,178 @@ from sampy.math import logn, _handle_zeros_in_scale
 
 
 class Binomial(Discrete):
-	def __init__(self, n_trials=1, bias=0.5, seed=None):
+    def __init__(self, n_trials=1, bias=0.5, seed=None):
 
-		# safety check n_trials
-		if isinstance(n_trials, float):
-			warnings.warn("Number of trials should be an integer. Casting to int")
+        # safety check n_trials
+        if isinstance(n_trials, float):
+            warnings.warn("Number of trials should be an integer. Casting to int")
 
-		if np.round(n_trials).astype(int) != n_trials:
-			raise ValueError("Number of trials must be an integer value")
-		else:
-			n_trials = np.round(n_trials).astype(int)
+        if np.round(n_trials).astype(int) != n_trials:
+            raise ValueError("Number of trials must be an integer value")
+        else:
+            n_trials = np.round(n_trials).astype(int)
 
-		# safety check bias
-		if bias < 0 or bias > 1:
-			raise ValueError("Bias must be between [0, 1]")
+        # safety check bias
+        if bias < 0 or bias > 1:
+            raise ValueError("Bias must be between [0, 1]")
 
-		self.n_trials = n_trials
-		self.bias = bias
-		self.seed = seed
-		self._state = self._set_random_state(seed)
+        self.n_trials = n_trials
+        self.bias = bias
+        self.seed = seed
+        self._state = self._set_random_state(seed)
 
-	@classmethod
-	def from_data(self, X, seed=None):
-		dist = Binomial(seed=seed)
-		return dist.fit(X)
+    @classmethod
+    def from_data(self, X, seed=None):
+        dist = Binomial(seed=seed)
+        return dist.fit(X)
 
-	def fit(self, X):
-		self._reset()
-		return self.partial_fit(X)
+    def fit(self, X):
+        self._reset()
+        return self.partial_fit(X)
 
-	def partial_fit(self, X):
-		# check array for numpy structure
-		X = check_array(X, reduce_args=True, ensure_1d=True).astype(float)
+    def partial_fit(self, X):
+        # check array for numpy structure
+        X = check_array(X, reduce_args=True, ensure_1d=True).astype(float)
 
-		# identify values outside of support 
-		# NOTE: we don't know the "true" upper bounds so we only
-		# check that values are positive
-		invalid = X < 0
-		X[invalid] = np.nan
+        # identify values outside of support
+        # NOTE: we don't know the "true" upper bounds so we only
+        # check that values are positive
+        invalid = X < 0
+        X[invalid] = np.nan
 
-		# first fit
-		if not hasattr(self, '_n_samples'):
-			self._n_samples = 0
+        # first fit
+        if not hasattr(self, "_n_samples"):
+            self._n_samples = 0
 
-		if self._mean is None and self._variance is None:
-			self._n_samples += X.shape[0] - np.isnan(X).sum()
-			self._mean = np.nanmean(X)
-			self._variance = np.nanvar(X)
-		else:
-			# previous values
-			prev_size = self._n_samples
-			prev_mean = self._mean
-			prev_variance = self._variance
+        if self._mean is None and self._variance is None:
+            self._n_samples += X.shape[0] - np.isnan(X).sum()
+            self._mean = np.nanmean(X)
+            self._variance = np.nanvar(X)
+        else:
+            # previous values
+            prev_size = self._n_samples
+            prev_mean = self._mean
+            prev_variance = self._variance
 
-			# new values
-			curr_size = X.shape[0] - np.isnan(X).sum()
-			curr_mean = np.nanmean(X)
-			curr_variance = np.nanvar(X)
+            # new values
+            curr_size = X.shape[0] - np.isnan(X).sum()
+            curr_mean = np.nanmean(X)
+            curr_variance = np.nanvar(X)
 
-			# update size
-			self._n_samples = prev_size + curr_size
+            # update size
+            self._n_samples = prev_size + curr_size
 
-			# update mean
-			self._mean = ((prev_mean * prev_size) + \
-				(curr_mean * curr_size)) / self._n_samples
+            # update mean
+            self._mean = (
+                (prev_mean * prev_size) + (curr_mean * curr_size)
+            ) / self._n_samples
 
-			# update variance
-			self._variance = ((prev_variance * prev_size) + \
-				(curr_variance * curr_size)) / self._n_samples
+            # update variance
+            self._variance = (
+                (prev_variance * prev_size) + (curr_variance * curr_size)
+            ) / self._n_samples
 
-		self.bias = 1 - (self._variance / self._mean)
-		self.n_trials = np.round(self._mean / self.bias).astype(int)
-		return self
+        self.bias = 1 - (self._variance / self._mean)
+        self.n_trials = np.round(self._mean / self.bias).astype(int)
+        return self
 
-	def sample(self, *size):
-		return self._state.binomial(self.n_trials, self.bias, size=size)
+    def sample(self, *size):
+        return self._state.binomial(self.n_trials, self.bias, size=size)
 
-	def pmf(self, *X):
-		# check array for numpy structure
-		X = check_array(X, reduce_args=True, ensure_1d=True)
-		return np.exp(self.log_pmf(X))
+    def pmf(self, *X):
+        # check array for numpy structure
+        X = check_array(X, reduce_args=True, ensure_1d=True)
+        return np.exp(self.log_pmf(X))
 
-	def log_pmf(self, *X):
-		# check array for numpy structure
-		X = check_array(X, reduce_args=True, ensure_1d=True)
-		
-		# Floor values of X
-		X = np.floor(X)
+    def log_pmf(self, *X):
+        # check array for numpy structure
+        X = check_array(X, reduce_args=True, ensure_1d=True)
 
-		# Expand all components of log-pmf
-		out = sc.gammaln(self.n_trials + 1) - \
-			(sc.gammaln(X + 1) + sc.gammaln(self.n_trials - X + 1)) + \
-			sc.xlogy(X, self.bias) + sc.xlog1py(self.n_trials - X, -self.bias)
-		return out
+        # Floor values of X
+        X = np.floor(X)
 
-	def cdf(self, *X):
-		# check array for numpy structure
-		X = check_array(X, reduce_args=True, ensure_1d=True)
+        # Expand all components of log-pmf
+        out = (
+            sc.gammaln(self.n_trials + 1)
+            - (sc.gammaln(X + 1) + sc.gammaln(self.n_trials - X + 1))
+            + sc.xlogy(X, self.bias)
+            + sc.xlog1py(self.n_trials - X, -self.bias)
+        )
+        return out
 
-		# floor X values
-		X = np.floor(X)
-		
-		return sc.bdtr(X, self.n_trials, self.bias)
+    def cdf(self, *X):
+        # check array for numpy structure
+        X = check_array(X, reduce_args=True, ensure_1d=True)
 
-	def log_cdf(self, *X):
-		# check array for numpy structure
-		X = check_array(X, reduce_args=True, ensure_1d=True)
-		
-		return np.log(self.cdf(X))
+        # floor X values
+        X = np.floor(X)
 
-	def quantile(self, *q):
-		# check array for numpy structure
-		q = check_array(q, reduce_args=True, ensure_1d=True)
+        return sc.bdtr(X, self.n_trials, self.bias)
 
-		vals = np.ceil(sc.bdtrik(q, self.n_trials, self.bias))
-		vals1 = np.maximum(vals - 1, 0)
-		temp = sc.bdtr(vals1, self.n_trials, self.bias)
-		return np.where(temp >= q, vals1, vals).astype(int)
+    def log_cdf(self, *X):
+        # check array for numpy structure
+        X = check_array(X, reduce_args=True, ensure_1d=True)
 
-	@property
-	def mean(self):
-		return self.n_trials * self.bias
+        return np.log(self.cdf(X))
 
-	@property
-	def median(self):
-		return self.quantile(0.5)
+    def quantile(self, *q):
+        # check array for numpy structure
+        q = check_array(q, reduce_args=True, ensure_1d=True)
 
-	@property
-	def mode(self):
-		return self.median
+        vals = np.ceil(sc.bdtrik(q, self.n_trials, self.bias))
+        vals1 = np.maximum(vals - 1, 0)
+        temp = sc.bdtr(vals1, self.n_trials, self.bias)
+        return np.where(temp >= q, vals1, vals).astype(int)
 
-	@property
-	def variance(self):
-		return self.n_trials * self.bias * (1 - self.bias)
+    @property
+    def mean(self):
+        return self.n_trials * self.bias
 
-	@property
-	def skewness(self):
-		n, p, q = self.n_trials, self.bias, 1 - self.bias
-		return (q - p) / np.sqrt(n * p * q)
+    @property
+    def median(self):
+        return self.quantile(0.5)
 
-	@property
-	def kurtosis(self):
-		n, p, q = self.n_trials, self.bias, 1 - self.bias
-		return (1 - (6 * p * q)) / (n * p * q)
+    @property
+    def mode(self):
+        return self.median
 
-	@property
-	def entropy(self):
-		n, p, q = self.n_trials, self.bias, 1 - self.bias
-		return 0.5 * logn(2 * np.pi * np.exp(1) * n * p * q, 2)
+    @property
+    def variance(self):
+        return self.n_trials * self.bias * (1 - self.bias)
 
-	@property
-	def perplexity(self):
-		return np.exp(self.entropy)
+    @property
+    def skewness(self):
+        n, p, q = self.n_trials, self.bias, 1 - self.bias
+        return (q - p) / np.sqrt(n * p * q)
 
-	@cache_property
-	def support(self):
-		return Interval(0, self.n_trials, True, True)
+    @property
+    def kurtosis(self):
+        n, p, q = self.n_trials, self.bias, 1 - self.bias
+        return (1 - (6 * p * q)) / (n * p * q)
 
-	def _reset(self):
-		if hasattr(self, '_n_samples'):
-			del self._n_samples
-		self.n_trials = None
-		self.bias = None
-		self._mean = None
-		self._variance = None
+    @property
+    def entropy(self):
+        n, p, q = self.n_trials, self.bias, 1 - self.bias
+        return 0.5 * logn(2 * np.pi * np.exp(1) * n * p * q, 2)
 
-	def __str__(self):
-		return f"Binomial(n_trials={self.n_trials}, bias={self.bias})"
+    @property
+    def perplexity(self):
+        return np.exp(self.entropy)
 
-	def __repr__(self):
-		return self.__str__()
+    @cache_property
+    def support(self):
+        return Interval(0, self.n_trials, True, True)
+    def _reset(self):
+        if hasattr(self, "_n_samples"):
+            del self._n_samples
+        self.n_trials = None
+        self.bias = None
+        self._mean = None
+        self._variance = None
+
+    def __str__(self):
+        return f"Binomial(n_trials={self.n_trials}, bias={self.bias})"
+
+    def __repr__(self):
+        return self.__str__()
+
