fc27675114e96003f0554014de976484e570a9a7
Pablo Romano <pablo.romano42@gmail.com>
2020-05-28 08:32:22 -0500

    Added html logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#
+html_logo = "../img/logo_64x64.png"
