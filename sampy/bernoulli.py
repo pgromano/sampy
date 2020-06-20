23e23dded333c05bcc200b12428ccbe3709e2a24
Pablo Romano <pablo.romano42@gmail.com>
2020-06-20 14:24:00 -0500

    Removed invalid argument with options to supress or warn. If the domain is invalid method should fail

--- sampy/bernoulli.py
+++ sampy/bernoulli.py
@@ -4,8 +4,11 @@ import scipy.special as sc
 
 from sampy.distributions import Discrete
 from sampy.interval import Interval
-from sampy.utils import check_array, cache_property
+from sampy.utils import check_array, cache_property, get_param_permutations, reduce_shape
 from sampy.math import _handle_zeros_in_scale
+
+
+__all__ = ['Bernoulli']
 
 
 class Bernoulli(Discrete):
@@ -29,24 +32,20 @@ class Bernoulli(Discrete):
 		and an integer will set the seed directly. Strings will be hashed to an
 		integer representation which can be a helpful description of the 
 		distribution use or associated experiment. 
-	invalid : str, {'error', 'warn', 'ignore'}
-		How the fit method should handle out of domain errors or other invalid
-		values. On 'error' the method will return ValueError and end. On 'warn'
-		a warning will notify the user of the issue and actions taken to 
-		mitigate. On 'ignore' the user is neither informed nor does the code
-		error out, proceed with caution.
 	"""
 
-    def __init__(self, bias=0.5, invalid="error", seed=None):
+    __slots__ = {
+        'bias': 'Bias in favor of positive event', 
+        'seed': 'Random number generator seed'
+    }
+
+    def __init__(self, bias=0.5,seed=None):
         self.bias = bias
         self.seed = seed
-        if invalid not in {"error", "warn", "ignore"}:
-            raise ValueError(f"Unable to interpret `invalid = {invalid}`")
-        self.invalid = invalid
         self._state = self._set_random_state(seed)
 
     @classmethod
-    def from_data(self, X, invalid="error", seed=None):
+    def from_data(self, X, seed=None):
         """Initialize distribution from data
 
         Parameters
@@ -54,12 +53,6 @@ class Bernoulli(Discrete):
         X : numpy.ndarray
             1D dataset which falls within the domain of the given distribution
             support. The Bernoulli distribution expects series of 0 or 1 only.
-        invalid : str, {'error', 'warn', 'ignore'}
-            How the fit method should handle out of domain errors or other invalid
-            values. On 'error' the method will return ValueError and end. On 'warn'
-            a warning will notify the user of the issue and actions taken to 
-            mitigate. On 'ignore' the user is neither informed nor does the code
-            error out, proceed with caution.
         seed : int, str or None, optional
             The seed by which to set the random number generator for the 
             `sample` method, by default None. A seed set to None will use the
@@ -74,11 +67,14 @@ class Bernoulli(Discrete):
             The fitted Bernoulli distribution model 
         """
 
-        dist = Bernoulli(invalid=invalid, seed=seed)
+        dist = Bernoulli(seed=seed)
         return dist.fit(X)
 
     def fit(self, X):
         """Fit model to data
+
+        Parameters are fit using a method of moments estimation. The bias
+        parameter can be estimated from the empirical mean sampled data.
 
         Parameters
         ----------
@@ -115,28 +111,20 @@ class Bernoulli(Discrete):
 
         # check array for numpy structure
         X = check_array(X, reduce_args=True, ensure_1d=True)
-        if X.dtype != int:
-            if self.invalid == "error":
-                raise ValueError(
-                    "Bernoulli distribution must be dtype intbut found {X.dtype}"
-                )
-            elif self.invalid == "warn":
-                warnings.warn(
-                    f"Bernoulli distribution should be dtype int but found {X.dtype}. Attempting to cast to int",
-                    category=UserWarning,
-                )
+        if X.dtype.kind != 'i':
+            warnings.warn(
+                f"Bernoulli distribution should be dtype int but found {X.dtype}. Attempting to cast to int",
+                category=UserWarning,
+            )
         X = X.astype(float)
 
         # identify values outside of support
         invalid = (1 - self.support.contains(X)).astype(bool)
         if np.sum(invalid) > 0:
-            if self.invalid == "error":
-                raise ValueError("Bernoulli must be 0 or 1 values only")
-            elif self.invalid == "warn":
-                warnings.warn(
-                    f"Training data outside support {self.support}. Will train ignoring values outside domain",
-                    category=UserWarning,
-                )
+            warnings.warn(
+                f"Training data outside support {self.support}. Will train ignoring values outside domain",
+                category=UserWarning,
+            )
         X[invalid] = np.nan
 
         # first fit
@@ -332,7 +320,7 @@ class Bernoulli(Discrete):
     def quantile(self, *q):
         """Quantile Function
 
-        Also known as the inverse cumulative Distribution function, this function
+        Also known as the inverse cumulative distribution function, this function
         takes known quantiles and returns the associated `X` value from the 
         support domain.
 
@@ -410,3 +398,182 @@ class Bernoulli(Discrete):
 
     def __repr__(self):
         return self.__str__()
+
+
+def _create_permutations(X, bias):
+    grid = np.meshgrid(X, bias, indexing='ij')
+    X = grid[0].ravel()
+    bias = grid[1].ravel()
+    return X, bias, grid[0].shape
+
+
+def pmf(X, bias, return_log=False, keepdims=False):
+    """Probability Mass Function
+
+    The probability mass function for the Bernoulli distribution is given
+    by two cases. 
+
+    .. math::
+        \begin{cases}
+            1-p  &\text{if } X = 0\\
+            p    &\text{if } X = 1
+        \end{cases}
+
+    where `p` is the :code:`bias` in favor of a positive event
+
+    Parameters
+    ----------
+    X : numpy.ndarray, int
+        1D dataset which falls within the domain of the given distribution
+        support. The Bernoulli distribution expects series of 0 or 1 only.
+        This value is often denoted `k` in the literature.
+    bias : float, optional
+        The bias or the likelihood of a positive event, by default 0.5. 
+        The values within this method assume that negative class will be 0
+        and positive class 1. 
+    return_log : bool, optional
+        Whether or not to return the log-transform
+    keepdims : bool, optional
+        If this is set to True, the axes which are reduced are left
+        in the result as dimensions with size one. With this option,
+        the result will broadcast correctly against the input array.
+
+    Returns
+    -------
+    numpy.ndarray
+        The output probability mass reported elementwise with respect to the
+        input data.
+    """
+
+    # check array for numpy structure
+    X, bias, shape = _create_permutations(X, bias)
+
+    # probability mass
+    out = np.where(X == 1, bias, 1 - bias)
+
+    # check bounds
+    out = np.where(out > 1, out, np.nan)
+
+    if return_log:
+        out = np.log(out)
+    out = out.reshape(shape)
+
+    if not keepdims:
+        out = out.squeeze()
+
+    if out.size == 1:
+        return np.asscalar(out)
+    return out
+
+
+def cdf(X, bias, return_log=False, keepdims=False):
+    """Cumulative Distribution Function
+
+    The cumulative distribution function for the Bernoulli distribution is 
+    given by three cases. 
+
+    .. math::
+        \begin{cases}
+            0      &\text{if } X \leq 0
+            1 - p  &\text{if } 0 \leq X \lt 1\\
+            1      &\text{if } X \geq 1
+        \end{cases}
+
+    where `p` is the :code:`bias` in favor of a positive event
+
+    Parameters
+    ----------
+    X : numpy.ndarray, int
+        1D dataset which falls within the domain of the given distribution
+        support. The Bernoulli distribution expects series of 0 or 1 only.
+        This value is often denoted `k` in the literature.
+    bias : float, optional
+        The bias or the likelihood of a positive event, by default 0.5. 
+        The values within this method assume that negative class will be 0
+        and positive class 1. 
+    return_log : bool, optional
+        Whether or not to return the log-transform
+    keepdims : bool, optional
+        If this is set to True, the axes which are reduced are left
+        in the result as dimensions with size one. With this option,
+        the result will broadcast correctly against the input array.
+
+    Returns
+    -------
+    numpy.ndarray
+        The output probability mass reported elementwise with respect to the
+        input data.
+    """
+
+    # check array for numpy structure
+    X, bias, shape = _create_permutations(X, bias)
+
+    # when X in bounds: 1 - p, else 0
+    in_bounds = np.logical_and(out <= 1, out >= 0)
+    out = np.where(in_bounds, 1 - bias, 0)
+
+    # when X >= 1 then 1
+    out = np.where(X >= 1, 1, out)
+    return out
+
+    if return_log:
+        out = np.log(out)
+    out = out.reshape(shape)
+
+    if not keepdims:
+        out = out.squeeze()
+
+    if out.size == 1:
+        return np.asscalar(out)
+    return out
+
+
+def quantile(self, q, bias, return_log=False, keepdims=False):
+    """Quantile Function
+
+    Also known as the inverse cumulative distribution function, this function
+    takes known quantiles and returns the associated `X` value from the 
+    support domain.
+
+    .. math::
+        \begin{cases}
+            0      &\text{if } 0 \leq q \lt p
+            1      &\text{if } p \leq q \lt 1
+        \end{cases}
+
+    Parameters
+    ----------
+    q : numpy.ndarray, float
+        The probabilities within domain [0, 1]
+    bias : float, optional
+        The bias or the likelihood of a positive event, by default 0.5. 
+        The values within this method assume that negative class will be 0
+        and positive class 1. 
+    return_log : bool, optional
+        Whether or not to return the log-transform
+    keepdims : bool, optional
+        If this is set to True, the axes which are reduced are left
+        in the result as dimensions with size one. With this option,
+        the result will broadcast correctly against the input array.
+
+    Returns
+    -------
+    numpy.ndarray
+        The `X` values from the support domain associated with the input
+        quantiles.
+    """
+    # check array for numpy structure
+    q, bias, shape = _create_permutations(q, bias)
+
+    # compute incomplete beta function
+    out = np.ceil(sc.bdtrik(q, 1, bias))
+
+    # filter bounds 
+    out = np.where(bias >= q, 0, out)
+
+    if not keepdims:
+        out = out.squeeze()
+
+    if out.size == 1:
+        return np.asscalar(out)
+    return out
